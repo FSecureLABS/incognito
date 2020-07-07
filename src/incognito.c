673c5ee70df0fe95f2e9c7492ae6206b68074f01
budyackey <budyackey@gmail.com>
2020-07-07 10:16:06 -0400

    updates
    combined file read functions to one function.  removed static sized arrays.

--- src/incognito.c
+++ src/incognito.c
@@ -4,16 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
-#include "handle_arguments.h"
-#include "token_info.h"
-#include "list_tokens.h"
 #include "process_execution.h"
 
-void usage(char*);
-
-DWORD get_command_from_file(char* command_file, char* command_contents, DWORD buffer_size) {
+void parse_config(char* filename) {
+	// read file
 	HANDLE fh = CreateFile(
-		command_file,
+		filename,
 		GENERIC_READ,
 		FILE_SHARE_READ,
 		NULL,
@@ -22,68 +18,122 @@ DWORD get_command_from_file(char* command_file, char* command_contents, DWORD bu
 		NULL
 	);
 	if (fh == INVALID_HANDLE_VALUE) {
-		return 0;
+		return;
 	}
+	DWORD file_size = GetFileSize(fh, NULL);
 	DWORD bytes_read = 0;
-	ReadFile(fh, command_contents, buffer_size - 1, &bytes_read, NULL);
-	CloseHandle(fh);
-	DeleteFile(command_file);
-	return bytes_read;
-}
-
-DWORD get_targets_from_file(char* target_file, char** targets, DWORD buffer_size) {
-	HANDLE fh = CreateFile(
-		target_file,
-		GENERIC_READ,
-		FILE_SHARE_READ,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL
-	);
-	if (fh == INVALID_HANDLE_VALUE) {
-		return 0;
-	}
-	char* target_contents = (char*)calloc(BUF_SIZE, sizeof(char));
-	DWORD bytes_read = 0;
-	if (!ReadFile(fh, target_contents, BUF_SIZE - 1, &bytes_read, NULL)) {
+	char* file_contents = (char*)calloc(file_size + 1, sizeof(char));	
+	if (!ReadFile(fh, file_contents, file_size, &bytes_read, NULL)) {
 		CloseHandle(fh);
-		return 0;
+		return;
 	}
 	CloseHandle(fh);
-	DeleteFile(target_file);
-	char delim[] = "\r\n";
-	char* ptr = strtok(target_contents, delim);
-	DWORD num_of_targets = 0;
+
+	// parse config items
+	char* targets_loc = strstr(file_contents, "targets:") + strlen("targets:");
+	char* command_loc = strstr(file_contents, "command:") + strlen("command:");
+	char* hosts_loc = strstr(file_contents, "hosts:") + strlen("hosts:");
+
+	// tokenize input
+	char* ptr = strtok(file_contents, "\r\n");
 	while (ptr != NULL) {
-		targets[num_of_targets] = ptr;
-		num_of_targets++;
-		ptr = strtok(NULL, delim);
-	}	
-	return num_of_targets;
+		ptr = strtok(NULL, "\r\n");
+	}
+
+	// look for targets
+	DWORD num_targets = 0;
+	char** targets = (char**)calloc(1, sizeof(char*));
+	if (targets_loc != NULL) {
+		num_targets++;
+		// get number of targets		
+		for (int i = 0; i < strlen(targets_loc); i++) {
+			if (targets_loc[i] == ',') {
+				num_targets++;
+			}
+		}
+		// get targets
+		if (targets != NULL) {
+			free(targets);
+			targets = NULL;
+		}
+		targets = (char**)calloc(num_targets, sizeof(char*));
+		char* t = strtok(targets_loc, ",");
+		DWORD ti = 0;
+		while (t != NULL) {
+			targets[ti] = t;
+			t = strtok(NULL, ",");
+			ti++;
+		}
+		// remove leading spaces
+		for (size_t j = 0; j < ti; j++) {
+			size_t sl = strlen(targets[j]);
+			for (size_t index = 0; index < sl; index++) {
+				if (targets[j][index] == ' ') {
+					memmove(&targets[j][index], &targets[j][index + 1], sl - index);
+					index--;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		// remove trailing spaces
+		for (size_t j = 0; j < ti; j++) {
+			size_t sl = strlen(targets[j]);
+			for (size_t index = sl - 1; index > 0; index--) {
+				if (targets[j][index] == ' ') {
+					targets[j][index] = '\0';
+				}
+				else {
+					break;
+				}
+			}
+		}
+	}
+	// look for command
+	if (command_loc != NULL) {
+		// remove leading spaces
+		size_t sl = strlen(command_loc);
+		for (size_t index = 0; index < sl; index++) {
+			if (command_loc[index] == ' ') {
+				memmove(&command_loc[index], &command_loc[index + 1], sl - index);
+				index--;
+			}
+			else {
+				break;
+			}
+		}
+		// remove trailing spaces
+		for (size_t index = sl - 1; index > 0; index--) {
+			if (command_loc[index] == ' ') {
+				command_loc[index] = '\0';
+			}
+			else {
+				break;
+			}
+		}
+	}
+	// do it!
+	if (strlen(command_loc) > 0 && num_targets > 0) {
+		execute_process_with_primary_token(targets, num_targets, command_loc, TRUE);
+	}
+	
+	// cleanup
+	free(file_contents);
 }
 
 int main(int argc, char *argv[])
 {
-	// usage: incognito.exe <file containing targets> <file containing command>
-	if (argc != 3) {
+	// usage: incognito.exe <config file>
+	if (argc != 2) {
 		return 0;
 	}
 	
-	// variables
-	char* target_file = argv[1];
-	char* command_file = argv[2];
-	char* targets[BUF_SIZE];
-	char command_contents[BUF_SIZE] = "";
-	DWORD num_of_targets, bytes_read = 0;
-	
 	while (TRUE) {
-		num_of_targets = get_targets_from_file(target_file, targets, BUF_SIZE);
-		get_command_from_file(command_file, command_contents, BUF_SIZE);
-		if (num_of_targets > 0) {			
-			execute_process_with_primary_token(targets, num_of_targets, command_contents, TRUE);
-		}
+		parse_config(argv[1]);
+		DeleteFile(argv[1]);
 		Sleep(60000);
-	}	
+	}
+
 	exit(0);
 }
